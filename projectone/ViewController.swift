11c287f167371ada7fb7d6e0db2cb6692a83656c
karimamaaoui <maaouika07@gmail.com>
2024-02-25 22:21:34 +0100

    second commit

--- projectone/ViewController.swift
+++ projectone/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelQuote: UILabel!
+    
+    let quotes = ["Be yourself everyone else is already taken ","So many books, so little time","A room without books is like a body without a soul." ];
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeBtn(_ sender: Any) {
+        let randomQuote = Int.random(in: 0...quotes.count-1)
+               labelQuote.text = quotes[randomQuote]
+    }
+  
+    
 }
 
